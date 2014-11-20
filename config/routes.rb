02c513d38da640bd2ca5aba8681998454a11da00
Bruno Hashimoto <bhashimoto@poli.ufrj.br>
2014-11-20 01:37:04 -0200

    Generated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,130 @@
 Rails.application.routes.draw do
+  get 'notification/index'
+
+  get 'notification/new'
+
+  get 'notification/create'
+
+  get 'notification/edit'
+
+  get 'notification/update'
+
+  get 'notification/destroy'
+
+  get 'notification/show'
+
+  get 'answer/index'
+
+  get 'answer/new'
+
+  get 'answer/create'
+
+  get 'answer/edit'
+
+  get 'answer/update'
+
+  get 'answer/destroy'
+
+  get 'answer/show'
+
+  get 'topic/index'
+
+  get 'topic/new'
+
+  get 'topic/create'
+
+  get 'topic/edit'
+
+  get 'topic/update'
+
+  get 'topic/destroy'
+
+  get 'topic/show'
+
+  get 'question/index'
+
+  get 'question/new'
+
+  get 'question/create'
+
+  get 'question/edit'
+
+  get 'question/update'
+
+  get 'question/destroy'
+
+  get 'question/show'
+
+  get 'subscription/index'
+
+  get 'subscription/new'
+
+  get 'subscription/create'
+
+  get 'subscription/edit'
+
+  get 'subscription/update'
+
+  get 'subscription/destroy'
+
+  get 'subscription/show'
+
+  get 'event/index'
+
+  get 'event/new'
+
+  get 'event/create'
+
+  get 'event/edit'
+
+  get 'event/update'
+
+  get 'event/destroy'
+
+  get 'event/show'
+
+  get 'classroom/index'
+
+  get 'classroom/new'
+
+  get 'classroom/create'
+
+  get 'classroom/edit'
+
+  get 'classroom/update'
+
+  get 'classroom/destroy'
+
+  get 'classroom/show'
+
+  get 'registration/index'
+
+  get 'registration/new'
+
+  get 'registration/create'
+
+  get 'registration/edit'
+
+  get 'registration/update'
+
+  get 'registration/destroy'
+
+  get 'registration/show'
+
+  get 'user/index'
+
+  get 'user/new'
+
+  get 'user/create'
+
+  get 'user/edit'
+
+  get 'user/update'
+
+  get 'user/destroy'
+
+  get 'user/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
